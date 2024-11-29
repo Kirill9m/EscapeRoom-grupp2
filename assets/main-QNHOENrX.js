d5b46f97c906f615cb16f2ceece5c8900c493804
Kirill9m <Kirill9m@users.noreply.github.com>
2024-11-29 22:51:02 +0000

    deploy: 38198b791de92b9dadbbe8e56520fb099ab0f56f

---
+++ assets/main-QNHOENrX.js
@@ -0,0 +1,2 @@
+import{f as o,r as g}from"./renderChallenges-BwSk_0S3.js";const e=document.querySelector(".challenges__list");let l=[];const i=async()=>{const{success:s,data:a,error:n}=await o();if(!s){console.error("Failed to fetch challenges:",n),e.innerHTML="<li>Error loading challenges. Please try again later.</li>";return}l=a;const r=[...l].sort((t,c)=>c.rating-t.rating).slice(0,3);g(r,e)};i();
+//# sourceMappingURL=main-QNHOENrX.js.map
